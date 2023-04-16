73b0516f02782f7bf763539170427f783c4752ec
eliJI <elijahingr@gmail.com>
2023-04-15 20:57:17 -0400

    fixed method leasing to loss in trailing line of data file

--- main.go
+++ main.go
@@ -1,79 +1,78 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"os"
-	"strings"
-	"flag"
-	"log"
 	"encoding/csv"
 	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
 )
 
 type Fault struct {
-
 	FaultType string
-	Address string
-
+	Address   string
 }
 
-//reduces duplicate pages, returning a shortened copy
-func reducePages(faults []Fault) []Fault {
+// reduces duplicate pages, returning a shortened copy
+func reduceDuplicates(faults []Fault) []Fault {
 	tempFaults := make([]Fault, 0)
 	i := 0
-	for  i < (len(faults)-1) {
-		tempFaults = append(tempFaults,faults[i])
+	for i < len(faults)-1 {
 		j := i
-		for faults[i] == faults[j] {
-			j++
+		for faults[i].Address == faults[j].Address && faults[i].FaultType == faults[j].FaultType {
+			j = j + 1
 		}
 		i = j
+		tempFaults = append(tempFaults, faults[i])
 	}
 	return tempFaults
 }
 
 func main() {
 
-	faults := make([]Fault,0)
+	faults := make([]Fault, 0)
 	pathPtr := flag.String("path", "default value", "file path to the data")
-
 	flag.Parse()
 	data, err := os.ReadFile(*pathPtr)
+
 	if err != nil {
 		panic(err)
 	}
+
 	content := string(data)
 	r := csv.NewReader(strings.NewReader(content))
+
 	for {
-		record, err := r.Read();
+		record, err := r.Read()
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
 			//fmt.Println(err)
 		}
-
-		if (record[1] != "" && record[0] != "") {
-			faults = append(faults,Fault{FaultType: record[0],Address: record[1]})
-
+		if record[1] != "" && record[0] != "" {
+			faults = append(faults, Fault{FaultType: record[0], Address: record[1]})
 		} else {
-			faults = append(faults,Fault{record[0],"na"})
-			
+			faults = append(faults, Fault{record[0], "na"})
 		}
-		
 	}
-		b, err := json.MarshalIndent(reducePages(faults),""," ")
 
-		if err != nil{
-			log.Fatal(err)
-		}
+	fmt.Println(len(faults))
+	b, err := json.MarshalIndent(reduceDuplicates(faults), "", " ")
 
-		err2 := os.WriteFile("data.json",b,0666)
-		if err2 != nil {
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		}
+	err2 := os.WriteFile("data.json", b, 0666)
 
-		fmt.Println("done")
-		fmt.Println(faults[0])
+	if err2 != nil {
+
+	}
+
+	fmt.Println("done")
+	fmt.Println(faults[0])
 }

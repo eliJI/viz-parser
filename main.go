d40bc9afa28e3ab80ebb093f168c5ab303d2a2cc
eliJI <elijahingr@gmail.com>
2023-04-16 22:46:45 -0400

    fixed bugs

--- main.go
+++ main.go
@@ -30,6 +30,30 @@ func reduceDuplicates(faults []Fault) []Fault {
 	}
 	tempFaults = append(tempFaults, faults[len(faults)-1])
 	return tempFaults
+
+}
+
+// returns ordred array of faults.
+// NOTE: our data seems to be in referenetial order already but this might not always be the case
+func getUnique(faults []Fault) []string {
+	uniqueAdresses := make([]string, 0)
+	//starts at 2 because of s at start
+	for i := 2; i < len(faults); i++ {
+		key := faults[i]
+		j := i - 1
+
+		for j >= 0 && key.Address < faults[j].Address {
+			faults[j+1] = faults[j]
+			j--
+		}
+		faults[j+1] = key
+	}
+
+	for i := 0; i < len(faults); i++ {
+		uniqueAdresses = append(uniqueAdresses, faults[i].Address)
+	}
+
+	return uniqueAdresses
 }
 
 func main() {
@@ -40,7 +64,7 @@ func main() {
 	data, err := os.ReadFile(*pathPtr)
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	content := string(data)
@@ -71,9 +95,25 @@ func main() {
 	err2 := os.WriteFile("data.json", b, 0666)
 
 	if err2 != nil {
-
+		log.Fatal(err2)
 	}
 
-	fmt.Println("done")
 	fmt.Println(faults[0])
+
+	uf, err3 := os.Create("unique.txt")
+	if err3 != nil {
+		log.Fatal(err3)
+	}
+
+	uniqueAdresses := getUnique(reduceDuplicates(faults))
+	for i := 0; i < len(uniqueAdresses); i++ {
+		line := uniqueAdresses[i] + "\n"
+		_, err := uf.Write([]byte(line))
+		if err != nil {
+			log.Fatal((err))
+		}
+
+	}
+	uf.Close()
+	fmt.Println("done")
 }
